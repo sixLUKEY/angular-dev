a5f77eb993031ddb6532722f0fd8f532cee85862
sixLUKEY <marcoslukerojo@gmail.com>
2024-10-22 18:26:10 +0200

    TS(#8) - Column updates

--- ticket-system/src/app/directives/column-color/column-color.directive.ts
+++ ticket-system/src/app/directives/column-color/column-color.directive.ts
@@ -7,10 +7,10 @@ import { Column } from '../../ticket-box/column/column.component';
 })
 export class ColumnColorDirective implements OnInit {
   private el: HTMLElement = inject(ElementRef).nativeElement;
-  @Input() appColumnColor!: Column;
+  @Input('appColumnColor') column!: Column;
   ngOnInit(): void {
-    this.el.style.backgroundColor = `${this.appColumnColor.primaryColor + this.appColumnColor.opacity}`;
-    this.el.style.border = this.appColumnColor.primaryColor;
+    this.el.style.backgroundColor = `${this.column.primaryColor + this.column.opacity}`;
+    this.el.style.border = this.column.primaryColor;
     this.el.style.borderWidth = '2px';
     this.el.style.borderStyle = 'dotted';
   }
